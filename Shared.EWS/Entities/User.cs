605c4d053d6a1e98013e00c8ed7ba7a7ea525ccd
Om Malaviya <om.malaviya@etatvasoft.com>
2026-05-07 16:16:34 +0530

    folder structure changes

---
+++ Shared.EWS/Entities/User.cs
@@ -0,0 +1,7 @@
+namespace Shared.EWS.Entities
+{
+    public class User
+    {
+        
+    }
+}
--- Shared.EWS/Filters/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Shared.HomeCare.Filters;
-
-public class Class1
-{
-
-}
